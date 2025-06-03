bb8a7752d2bbc97e786cd1c06cd9b591edf47257
FabiKogWi <liebisch.fabian@web.de>
2025-06-03 12:32:52 +0200

    Created working menu

--- main.py
+++ main.py
@@ -5,16 +5,65 @@ pygame.init()
 # info = pygame.display.Info()
 # WIDTH, HEIGHT = info.current_w, info.current_h
 screen = pygame.display.set_mode((1200, 600))
+clock = pygame.time.Clock()
 
-button = buttons.TextButton(screen.get_width() // 2, screen.get_height() // 2, 200, 400, "Hello")
+# Game States
+play = True
+menu = False
+dead = False
+
+# Menu Buttons
+options = buttons.TextButton(screen.get_width() // 2 - 100, screen.get_height() // 2 - 25, 200, 50, "Options")
+resume = buttons.TextButton(screen.get_width() // 2 - 100, screen.get_height() // 2 - 75, 200, 50, "Resume")
+help = buttons.TextButton(screen.get_width() // 2 - 100, screen.get_height() // 2 + 25, 200, 50, "Help")
+leave = buttons.TextButton(screen.get_width() // 2 - 100, screen.get_height() // 2 + 75, 200, 50, "Leave")
+
 
 while True:
-    button.draw(screen)
- 
+
+    if menu:
+        screen.fill("Dark Green")
+        options.draw(screen)
+        resume.draw(screen)
+        help.draw(screen)
+        leave.draw(screen)
+    if play:
+        screen.fill("Green")
+
     for event in pygame.event.get():
+        key_pressed = False # A bit of a weird solution
+        # Controlls
+        if play and not key_pressed:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print("clicked")
+                    menu = True
+                    play = False
+                    key_pressed = True
+        elif menu and not key_pressed:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print("noob")
+                    menu = False
+                    play = True
+                    key_pressed = True
+
         # Menu
-        if button.is_clicked(event):
-            print("Clicked :)")
+        if menu:
+            if options.is_clicked(event):
+                print("Options Clicked :)")
+            if help.is_clicked(event):
+                print("HEEEEEELPT")
+            if resume.is_clicked(event):
+                menu = False
+                play = True
+            if leave.is_clicked(event):
+                pygame.quit()
+                exit()
+        
+        # Quitting gracefully
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     pygame.display.update()
+    clock.tick(60)
