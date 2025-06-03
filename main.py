7c558fb2f7f65c0c55ecb5830fc267c8fc83a21b
FabiKogWi <liebisch.fabian@web.de>
2025-06-03 21:05:07 +0200

    Created two directional movement system, which is still bugged

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ player_surf.fill("Red")
 player_rect = player_surf.get_rect(center = (screen.get_width() // 2, screen.get_height() // 2))
 
 # Map Surface
-map_surf = pygame.Surface((1200, 600))
+map_surf = pygame.Surface((600, 600))
 map_surf.fill("Green")
 corners = pygame.Surface((100, 100))
 corners.fill("Brown")
@@ -34,6 +34,9 @@ map_surf.blit(corners, (1100, 0))
 map_surf.blit(corners, (1100, 500))
 map_surf.blit(corners, (0, 500))
 map_rect = map_surf.get_rect(topleft = (0, 0))
+tiles = int(screen.get_width() // map_surf.get_width()) + 1
+scroll = 0
+print(f'{tiles}')
 
 
 
@@ -45,23 +48,45 @@ while True:
         buttons.draw(screen)
         pygame.display.flip()
     if play:
-        screen.fill("Green")
+        # screen.fill("Green")
         # Draw scrolling background
-        screen.blit(map_surf, map_rect)
+        # screen.blit(map_surf, map_rect)
         screen.blit(player_surf, player_rect)
+
+    # for i in range(0, tiles):
+    #     screen.blit(map_surf, (i * map_surf.get_width() + scroll, 0))
+    # scroll -= 5
+
+    # if abs(scroll) > map_surf.get_width():
+    #     scroll = 0
 
 
 
     for event in pygame.event.get():
+
+        # Controls
         key_pressed = False # A bit of a weird solution
-        # Controlls
         if play and not key_pressed:
             if event.type == pygame.KEYDOWN:
+                # Menu
                 if event.key == pygame.K_ESCAPE:
                     print("clicked")
                     menu = True
                     play = False
                     key_pressed = True
+                # Map Navigation
+                if event.key == pygame.K_d:
+                    for i in range(0, tiles):
+                        screen.blit(map_surf, (i * map_surf.get_width() + scroll, 0))
+                    scroll -= 10
+                if event.key == pygame.K_a:
+                    for i in range(0, tiles):
+                        screen.blit(map_surf, (i * map_surf.get_width() + scroll, 0))
+                    scroll += 10
+
+                if abs(scroll) > map_surf.get_width() or scroll :
+                        scroll = 0
+            
         elif menu and not key_pressed:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -69,13 +94,14 @@ while True:
                     menu = False
                     play = True
                     key_pressed = True
+        
 
         # Menu
         if menu:
             if options.is_clicked(event):
                 print("Options Clicked :)")
             if help.is_clicked(event):
-                print("HEEEEEELPT")
+                print("HEEEEEELP")
             if resume.is_clicked(event):
                 menu = False
                 play = True
