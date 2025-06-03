4c4459a5171763208bfdc089cb31ba21e1569437
FabiKogWi <liebisch.fabian@web.de>
2025-06-03 11:24:08 +0200

    Changed screen dimensions back to 1200 * 600 to make developement easier

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import pygame
 import buttons
 
 pygame.init()
-info = pygame.display.Info()
-WIDTH, HEIGHT = info.current_w, info.current_h
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+# info = pygame.display.Info()
+# WIDTH, HEIGHT = info.current_w, info.current_h
+screen = pygame.display.set_mode((1200, 600))
 
-button = buttons.TextButton(100, 100, 400, 100, "Hello World")
+button = buttons.TextButton(screen.get_width() // 2, screen.get_height() // 2, 200, 400, "Hello")
 
 while True:
     button.draw(screen)
  
     for event in pygame.event.get():
+        # Menu
         if button.is_clicked(event):
             print("Clicked :)")
         if event.type == pygame.QUIT:
